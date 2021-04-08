98723f6872b5080864d8583f6bf341ca0afb54a9
ivan-bosak <50490744+ivan-bosak@users.noreply.github.com>
2021-04-08 13:40:03 +0300

    Skip null objects in JSON response

--- StringGenerator.WEB/Startup.cs
+++ StringGenerator.WEB/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StringGenerator.BLL.Handlers;
 using StringGenerator.BLL.Interfaces;
 using StringGenerator.BLL.Services;
 using System.Reflection;
@@ -16,11 +17,14 @@ namespace StringGenerator
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly(), Assembly.GetAssembly(typeof(StringGenerator.BLL.Handlers.StringGenerationHandler)));
             services.AddScoped<IStringGenerator, StringFactory>();
             services.AddScoped<IGeneratorService, StringGeneratorService>();
 
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.IgnoreNullValues = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
